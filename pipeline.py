a500e9992ed094e5d19c0603d85457c2909bf4ba
azabelo <azabelo21old@gmail.com>
2023-07-14 02:23:56 -0700

    supervised training

--- pipeline.py
+++ pipeline.py
@@ -195,7 +195,7 @@ class Resize_Transform():
         self.time_size = time_size
 
     def __call__(self, video):
-        return resize_video(video, self.space_size, self.time_size).permute(0,2,1,3) #permute to get CxTxHxW
+        return resize_video(video, self.space_size, self.time_size).permute(1,0,2,3) #permute to get CxTxHxW
 
 def supervised_train(model, path_to_hmdb51, args):
     print("starting sup training")
